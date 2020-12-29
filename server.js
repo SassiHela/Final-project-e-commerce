0c0b3bebe155afd415eb01e0ef33ea262570df2a
Hela Sassi <hela.sassi13@gmail.com>
2020-12-29 21:44:35 +0100

    changes

--- server.js
+++ server.js
@@ -36,6 +36,8 @@ app.post(
   "/create-checkout-session",
   asyncHandler(async (req, res) => {
     const { orderItems, user, _id } = req.body;
+    let success_url = "";
+    let cancel_url = "";
 
     const items = orderItems.map((product) => {
       return {
@@ -50,14 +52,21 @@ app.post(
       };
     });
 
+    if (process.env.NODE_ENV === "production") {
+      success_url = `https://tunisierecyclage.herokuapp.com/order/${_id}/pay`;
+      cancel_url = `https://tunisierecyclage.herokuapp.com/order/${_id}/cancel`;
+    } else {
+      success_url = `http://localhost:3000/order/${_id}/pay`;
+      cancel_url = `http://localhost:3000/order/${_id}/cancel`;
+    }
+
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: items,
       mode: "payment",
       //success_url: `https://tunisierecyclage.herokuapp.com/order/${_id}?session_id={CHECKOUT_SESSION_ID}`,
-
-      success_url: `http://localhost:3000/order/${_id}/pay`,
-      cancel_url: `http://localhost:3000/order/${_id}/cancel`,
+      success_url: success_url,
+      cancel_url: cancel_url,
       customer_email: user.email,
     });
 
